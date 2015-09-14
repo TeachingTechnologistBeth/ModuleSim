3375f412d40ce8982ef2d272e8e0771d01a87680
Alistair <aw12700@my.bristol.ac.uk>
2015-09-14 14:00:03 +0100

    Version doc

---
+++ src/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * ModuleSim. Created by awick on 14/09/2015.
+ *
+ * @Version 0.1.0
+ */
--- src/simulator/Main.java
+++ src/simulator/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	public static GUI ui = null;
 	public static Sim sim = null;
-	
+
 	/**
 	 * Program starting point
 	 * @param args System argument
@@ -27,23 +27,18 @@ public class Main {
 				ui = new GUI();
 				ui.generateUI();
 				ui.showUI(true);
-				
+
 				// Start render ticking
 				Thread t = new Thread(new Ticker());
 				t.start();
-				
-				// Start sim ticking
-				// N.B. sim is initialized below *before* this is called
+
+				// Start sim ticking - sim is initialized below *before* this is called
 				sim.start();
-				
-				// TEST show a memory editor
-				//MemEdit e = ui.newMemEdit();
-				//e.show((RAM) AvailableModules.RAM.getSrcModule().createNew());
 			}
 		});
-		
+
 		// Set up simulator
 		sim = new Sim();
 	}
-	
+
 }
