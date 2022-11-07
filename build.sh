3a1963a723b6062f53fd09c458a65b8f618a3204
Foxinatel <nathanielma5on@yahoo.com>
2022-11-07 23:48:40 +0000

    small patch to allow compiling on newer Java versions

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 mkdir ./out
 find -name "*.java" > sources.txt
-javac -d ./out @sources.txt
+BUILD_COMMAND="javac -d ./out @sources.txt"
+${BUILD_COMMAND} --add-exports=java.desktop/sun.awt=ALL-UNNAMED || \
+  echo "Command failed, retrying assuming older Java build" && ${BUILD_COMMAND}
 cd ./out
 find ../src -name "*.png" -exec cp '{}' ./com/modsim/res/ \;
 jar cfm ../ModuleSim-Test.jar ../src/META-INF/MANIFEST.MF ./
